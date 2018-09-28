4bf69dbd23e995875c9fabc971a72a73bc9c8e4e
aarongoddard <aaronbear@gmail.com>
2018-09-28 14:47:27 -0700

    Complete phase one

---
+++ widgets/frontend/widgets.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+document.addEventListener("DOMContentLoaded",(e) => {
+  const root = document.getElementById('root');
+  ReactDOM.render(
+    <div className="div1">
+      <h1>React is working</h1>
+    </div>,
+    root);
+});
